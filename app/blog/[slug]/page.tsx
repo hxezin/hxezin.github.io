0bc0c429b8a35b5b0228d24a90322b41263bc502
hxezin <leehxejin@gmail.com>
2023-08-21 23:56:35 +0900

    feat: dynamic metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,4 @@
-import Head from 'next/head';
-
+import type { Metadata, ResolvingMetadata } from 'next';
 import PostContent from '@/_components/post/post-detail/PostContent';
 import { getPostData, getPostFiles } from '@/_helpers/post';
 
@@ -10,6 +9,27 @@ export async function generateStaticParams() {
   const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ''));
 
   return slugs.map((slug) => ({ slug: slug }));
+}
+
+interface BlogMetadata {
+  params: { slug: string };
+}
+
+export async function generateMetadata(
+  { params }: BlogMetadata,
+  parent?: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
+
+  const postData = getPostData(slug);
+
+  return {
+    title: postData.title,
+    openGraph: {
+      title: postData.title,
+      description: postData.excerpt,
+    },
+  };
 }
 
 interface Props {
