ddc6d4d130b33e91b44b57c1fc1cdf40582a36ba
hxezin <leehxejin@gmail.com>
2023-08-24 23:11:47 +0900

    style: 글 수정

--- app/(home)/Hero.tsx
+++ app/(home)/Hero.tsx
@@ -1,8 +1,4 @@
 import Image from 'next/image';
-import { FiGithub, FiMail } from 'react-icons/fi';
-
-const contactClass =
-  'p-2 rounded-3xl hover:bg-purple-100 text-lg transition-color duration-300';
 
 function Hero() {
   return (
@@ -19,20 +15,7 @@ function Hero() {
         <div className='flex flex-col justify-center pl-2'>
           <h3 className='font-bold'>이혜진</h3>
           <p className='text-zinc-500 mb-3 text-base'>Frontend Engineer</p>
-          <p className='text-zinc-800'>배움과 성장, 수많은 시행착오 기록</p>
         </div>
-        <ul className='flex gap-2'>
-          <li className={contactClass}>
-            <a href='https://github.com/hxezin' target='_blank'>
-              <FiGithub />
-            </a>
-          </li>
-          <li className={contactClass}>
-            <a href='mailto:leehxejin@gmail.com' target='_blank'>
-              <FiMail />
-            </a>
-          </li>
-        </ul>
       </div>
     </section>
   );
