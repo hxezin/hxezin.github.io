98074cfc5981bab391e734b1c2e5fdeeb8908546
hxezin <leehxejin@gmail.com>
2023-08-31 22:48:13 +0900

    feat: metadata icon, apple icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,13 @@ export const metadata: Metadata = {
     title: '이혜진 블로그',
     description: '프론트엔드 이혜진 개발 블로그',
   },
+  icons: {
+    icon: '/icon.png',
+    apple: '/apple-icon.png',
+  },
+  verification: {
+    google: 'ajW_DDkVzItzz_bS6O3BI3DNJDcAj3NnAlwbfgAS3j4',
+  },
 };
 
 export default function RootLayout({
@@ -20,12 +27,6 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <AnalyticsProvider>
-        <head>
-          <meta
-            name='google-site-verification'
-            content='ajW_DDkVzItzz_bS6O3BI3DNJDcAj3NnAlwbfgAS3j4'
-          />
-        </head>
         <body suppressHydrationWarning={true}>
           <div className='wrapper h-auto min-h-full pb-28 max-w-4xl mx-auto px-4'>
             <Header />
