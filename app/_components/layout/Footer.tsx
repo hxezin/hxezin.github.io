99ca29c77b3ed5ea638eb721b4c75c83f6376d11
hxezin <leehxejin@gmail.com>
2023-09-24 15:47:40 +0900

    refactor: icon 통일

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,24 +1,21 @@
-import { BiLogoGithub, BiLogoGmail } from 'react-icons/bi';
-
-const contactClass = 'text-xl text-neutral-600';
+import { CONTACT } from '@/_helpers/about';
 
 function Footer() {
   return (
     <footer className='h-24 transform -translate-y-full border-t'>
       <div className='max-w-4xl h-full flex flex-col justify-center gap-1 px-4 mx-auto'>
         <ul className='flex gap-2'>
-          <li>
-            <a href='https://github.com/hxezin' target='_blank'>
-              <BiLogoGithub className='text-xl text-neutral-500' />
-            </a>
-          </li>
-          <li>
-            <a href='mailto:leehxejin@gmail.com' target='_blank'>
-              <BiLogoGmail className='text-xl text-neutral-500' />
-            </a>
-          </li>
+          {CONTACT.map((contact) => {
+            return (
+              <li key={contact.name}>
+                <a href={contact.href} target='_blank'>
+                  <contact.icon className='text-xl text-neutral-500' />
+                </a>
+              </li>
+            );
+          })}
         </ul>
-        <div className='text-sm text-neutral-500'>© 2023 Hyejin Lee</div>
+        <div className='text-sm text-neutral-500'>© 2023 hxezin </div>
       </div>
     </footer>
   );
--- app/_helpers/about.ts
+++ app/_helpers/about.ts
@@ -1,10 +1,10 @@
-import { AiFillGithub, AiOutlineMail } from 'react-icons/ai';
+import { BiLogoGithub, BiLogoGmail } from 'react-icons/bi';
 
 export const HEADER_CSS = 'inline font-bold shadow-[inset_0_-10px_0_#fcd34d]';
 
 export const CONTACT = [
-  { name: 'GitHub', href: 'https://github.com/hxezin', icon: AiFillGithub },
-  { name: 'Mail', href: 'mailto:leehxejin@gmail.com', icon: AiOutlineMail },
+  { name: 'GitHub', href: 'https://github.com/hxezin', icon: BiLogoGithub },
+  { name: 'Mail', href: 'mailto:leehxejin@gmail.com', icon: BiLogoGmail },
 ];
 
 export const SKILL = [
