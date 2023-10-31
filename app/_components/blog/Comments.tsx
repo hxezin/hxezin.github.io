b86ee1474d84ff2d0ea7d7543fe7a2b6c77f2566
hxezin <leehxejin@gmail.com>
2023-10-31 15:31:38 +0900

    feat: giscus에 theme 적용

--- app/_components/blog/Comments.tsx
+++ app/_components/blog/Comments.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { ThemeContext } from '@/_contexts/ThemeContext';
+import { useContext, useEffect, useRef } from 'react';
 
 export default function Comments() {
+  const { theme } = useContext(ThemeContext);
+
   const ref = useRef<HTMLDivElement>(null);
-  const theme = 'light_tritanopia';
 
   useEffect(() => {
-    if (!ref.current || ref.current.hasChildNodes()) return;
+    if (!ref.current) return;
 
     const scriptElem = document.createElement('script');
     scriptElem.src = 'https://giscus.app/client.js';
     scriptElem.async = true;
     scriptElem.crossOrigin = 'anonymous';
-
     scriptElem.setAttribute('data-repo', 'hxezin/hxezin.github.io');
     scriptElem.setAttribute('data-repo-id', 'R_kgDOKIGoOQ');
     scriptElem.setAttribute('data-category', 'Comments');
@@ -23,17 +24,18 @@ export default function Comments() {
     scriptElem.setAttribute('data-reactions-enabled', '1');
     scriptElem.setAttribute('data-emit-metadata', '0');
     scriptElem.setAttribute('data-input-position', 'top');
-    scriptElem.setAttribute('data-theme', theme);
+    scriptElem.setAttribute('data-theme', `${theme}_tritanopia`);
     scriptElem.setAttribute('data-lang', 'en');
+
     ref.current.appendChild(scriptElem);
-  }, []);
+  }, [theme]);
 
   useEffect(() => {
     const iframe = document.querySelector<HTMLIFrameElement>(
       'iframe.giscus-frame'
     );
     iframe?.contentWindow?.postMessage(
-      { giscus: { setConfig: { theme } } },
+      { giscus: { setConfig: { theme: `${theme}_tritanopia` } } },
       'https://giscus.app'
     );
   }, [theme]);
