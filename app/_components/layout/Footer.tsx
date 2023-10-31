db5b4146cfbf0322891a34c19638683b21d13554
hxezin <leehxejin@gmail.com>
2023-10-31 16:43:06 +0900

    style: css 변경

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -2,20 +2,20 @@ import { CONTACT } from '@/_helpers/about';
 
 function Footer() {
   return (
-    <footer className='h-24 transform -translate-y-full border-t'>
-      <div className='max-w-4xl h-full flex flex-col justify-center gap-1 px-4 mx-auto'>
+    <footer className='h-24 transform -translate-y-full border-t border-primary'>
+      <div className='max-w-4xl h-full flex flex-col justify-center gap-1 px-4 mx-auto text-secondary'>
         <ul className='flex gap-2'>
           {CONTACT.map((contact) => {
             return (
               <li key={contact.name}>
                 <a href={contact.href} target='_blank'>
-                  <contact.icon className='text-xl text-neutral-500' />
+                  <contact.icon className='text-xl' />
                 </a>
               </li>
             );
           })}
         </ul>
-        <div className='text-sm text-neutral-500'>© 2023 hxezin </div>
+        <div className='text-sm'>© 2023 hxezin </div>
       </div>
     </footer>
   );
