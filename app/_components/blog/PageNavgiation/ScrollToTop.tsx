67ebc9fb73f3e71a9274c2bdd0c0d1a51a482c3b
hxezin <leehxejin@gmail.com>
2025-11-01 13:48:07 +0900

    fix: ScrollToTop 컴포넌트 메모리 누수 수정
    - useEffect cleanup 함수에서 scroll 이벤트 리스너 제거

--- app/_components/blog/PageNavgiation/ScrollToTop.tsx
+++ app/_components/blog/PageNavgiation/ScrollToTop.tsx
@@ -5,13 +5,19 @@ const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    const handleScroll = () => {
       if (window.scrollY > 400) {
         setShowTopBtn(true);
       } else {
         setShowTopBtn(false);
       }
-    });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
   const goToTop = () => {
     window.scrollTo({
